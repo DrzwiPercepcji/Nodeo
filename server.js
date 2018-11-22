11939ef4412b4f3ce3588203a60ca5fbabd4bf51
admin <szynow95@gmail.com>
2018-11-22 14:15:03 +0100

    Hello World

---
+++ server.js
@@ -0,0 +1,22 @@
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+let express = require('express');
+let app = express();
+
+let mustache = require('mustache-express');
+
+app.engine('mustache', mustache());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.get('/', function (req, res)
+{
+	res.render('index', {
+		title: 'Hello World!'
+	});
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
