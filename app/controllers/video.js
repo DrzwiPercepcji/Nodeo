9d36ea0b738530f7b85460ce3464318f1640aa15
Szymon <szynow+dev@protonmail.com>
2019-08-05 19:27:11 +0200

    Encryption test

--- app/controllers/video.js
+++ app/controllers/video.js
@@ -116,38 +116,15 @@ exports.stream = function (req, res) {
     const path = APP_DATA + 'videos/' + req.params.videoId + '.mp4';
     const stat = fs.statSync(path);
     const fileSize = stat.size;
-    const range = req.headers.range;
 
-    if (range) {
-        console.log(range);
+    const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4'
+    };
 
-        const parts = range.replace(/bytes=/, '').split('-');
-        const start = parseInt(parts[0], 10);
+    res.writeHead(200, head);
 
-        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-        const chunksize = (end - start) + 1;
-        const file = fs.createReadStream(path, { start, end });
-
-        const head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4'
-        };
-
-        res.writeHead(206, head);
-        file.pipe(res);
-    }
-    else {
-        const head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4'
-        };
-
-        res.writeHead(200, head);
-        fs.createReadStream(path).pipe(res);
-    }
+    encryption.decryptFile(path, '1234123412341234', 0, fileSize - 1, res);
 };
 
 exports.thumb = function (request, response) {
--- app/modules/encryption.js
+++ app/modules/encryption.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
-function calculateRealStart() {
-}
+const DIRECTION_INCREASE = 1;
+const DIRECTION_DECREASE = -1;
 
-function calculateRealEnd() {
+function calculateByte(byte, direction) {
+    let result = byte;
+
+    while (result % 32 !== 0) {
+        result += direction;
+    }
+
+    return result;
 }
 
 exports.encryptFile = function (filePath, key) {
@@ -21,6 +28,39 @@ exports.encryptFile = function (filePath, key) {
     });
 }
 
-exports.decryptFile = function (filePath, key, start, end) {
+exports.decryptFile = function (path, key, start, end, response) {
+    console.log('Requested bytes from: ' + start + ' to: ' + end);
+
+    //let realStart = calculateByte(start, DIRECTION_DECREASE);
+    //let realEnd = calculateByte(end, DIRECTION_INCREASE) - 1;
+
+    let decipher = crypto.createDecipheriv('aes-128-ecb', key, '');
+
+    const reader = fs.createReadStream(path + '.aes');
+
+    reader.pipe(decipher).pipe(response);
+
+    /*
+    let encrypted;
     
+    reader.on('data', function (chunk) {
+        encrypted += chunk;
+    });
+
+    reader.on('end', function () {
+        console.log('Finished reading bytes from: ' + realStart + ' to: ' + realEnd);
+
+        let decrypted = decipher.update(encrypted, 'binary', 'utf8');
+
+        let sliceStart = start - realStart;
+        let sliceEnd = end - start + sliceStart;
+
+        decrypted.slice(sliceStart, sliceEnd);
+
+        console.log('Sliced bytes from: ' + sliceStart + ' to: ' + sliceEnd);
+
+        response.write(decrypted, 'binary');
+        response.end(null, 'binary');
+    });
+    */
 }
